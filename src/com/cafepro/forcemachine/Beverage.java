b3b59a8821ed4e958cca6aa70c48d3b359cc4f47
NoteBookMint <hodori991004@gmail.com>
2021-05-25 20:03:48 +0900

    Ready to BackProcess
    Create Beverage and Material Class (and Getter, Setter)

---
+++ src/com/cafepro/forcemachine/Beverage.java
@@ -0,0 +1,22 @@
+package com.cafepro.forcemachine;
+
+public class Beverage {
+    private String beverageName;
+    private int beveragePrice;
+
+    public String getBeverageName() {
+        return beverageName;
+    }
+
+    public void setBeverageName(String beverageName) {
+        this.beverageName = beverageName;
+    }
+
+    public int getBeveragePrice() {
+        return beveragePrice;
+    }
+
+    public void setBeveragePrice(int beveragePrice) {
+        this.beveragePrice = beveragePrice;
+    }
+}
---
+++ src/com/cafepro/forcemachine/Material.java
@@ -0,0 +1,23 @@
+package com.cafepro.forcemachine;
+
+public class Material {
+    private String materialName;
+    private int materialNum;
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public int getMaterialNum() {
+        return materialNum;
+    }
+
+    public void setMaterialNum(int materialNum) {
+        this.materialNum = materialNum;
+    }
+
+}
