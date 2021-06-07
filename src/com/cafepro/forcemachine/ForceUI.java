dda84983e195d2b9a0a0c32e04526970d3ae0ab2
NoteBookMint <hodori991004@gmail.com>
2021-06-07 14:43:51 +0900

    Add Button Event Basically
    just print what you press!

--- src/com/cafepro/forcemachine/ForceUI.java
+++ src/com/cafepro/forcemachine/ForceUI.java
@@ -13,8 +13,16 @@ public class ForceUI extends JFrame {
     private JPanel contentPane;
     private JTextField textField;
 
+    // Label
+    JLabel lblNewLabel = new JLabel("주문 없음");
     // Buttons
     JButton btn_stock = new JButton("재고 관리");
+    JButton americano = new JButton("아메리카노");
+    JButton essfresso = new JButton("에스프레소");
+    JButton choco_latte = new JButton("초코 라떼");
+    JButton caramel_latte = new JButton("카라멜 마끼아토");
+    JButton vanilla_latte = new JButton("바닐라 라떼");
+    JButton afogato = new JButton("아포가토");
 
     /**
      * Launch the application.
@@ -100,28 +108,17 @@ public class ForceUI extends JFrame {
         contentPane.add(panel_1, BorderLayout.CENTER);
         panel_1.setLayout(new GridLayout(0, 2, 0, 0));
 
-        JButton americano = new JButton("아메리카노");
+
         panel_1.add(americano);
-
-        JButton essfresso = new JButton("에스프레소");
         panel_1.add(essfresso);
-
-        JButton choco_latte = new JButton("초코 라떼");
         panel_1.add(choco_latte);
-
-        JButton caramel_latte = new JButton("카라멜 마끼아토");
         panel_1.add(caramel_latte);
-
-        JButton vanilla_latte = new JButton("바닐라 라떼");
         panel_1.add(vanilla_latte);
-
-        JButton afogato = new JButton("아포가토");
         panel_1.add(afogato);
 
         JPanel panel_2 = new JPanel();
         contentPane.add(panel_2, BorderLayout.WEST);
 
-        JLabel lblNewLabel = new JLabel("New label");
         panel_2.add(lblNewLabel);
         panel_2.setPreferredSize(new Dimension(200,400));
 
@@ -130,18 +127,40 @@ public class ForceUI extends JFrame {
         textField.setColumns(10);
 
         btn_stock.addActionListener(new EventHandler());
+        americano.addActionListener(new EventHandler());
+        essfresso.addActionListener(new EventHandler());
+        caramel_latte.addActionListener(new EventHandler());
+        choco_latte.addActionListener(new EventHandler());
+        afogato.addActionListener(new EventHandler());
+        vanilla_latte.addActionListener(new EventHandler());
+
+
     }
 
     class EventHandler implements ActionListener {
         // Edit material Stock
         @Override
         public void actionPerformed(ActionEvent e) {
-            setVisible(false);
-            try {
-                ForceSub forceSub = new ForceSub();
-                forceSub.ShowSub();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
+            if(e.getSource() == btn_stock) {
+                setVisible(false);
+                try {
+                    ForceSub forceSub = new ForceSub();
+                    forceSub.ShowSub();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            } else if(e.getSource() == americano) {
+                lblNewLabel.setText(americano.getText());
+            }else if(e.getSource() == essfresso) {
+                lblNewLabel.setText(essfresso.getText());
+            }else if(e.getSource() == choco_latte) {
+                lblNewLabel.setText(choco_latte.getText());
+            }else if(e.getSource() == caramel_latte) {
+                lblNewLabel.setText(caramel_latte.getText());
+            }else if(e.getSource() == vanilla_latte) {
+                lblNewLabel.setText(vanilla_latte.getText());
+            }else if(e.getSource() == afogato) {
+                lblNewLabel.setText(afogato.getText());
             }
         }
     }
